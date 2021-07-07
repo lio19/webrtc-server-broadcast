135ed86117aaace07047fdcb45a84dba7e5d9899
liquanqing <liquanqing110@qq.com>
2021-07-07 12:16:14 +0800

    [fet]添加跨域运行

--- room_server.go
+++ room_server.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"time"
 )
 
 type RoomServer struct {
 	ginEngine *gin.Engine
-	sfu *SFUServer
+	sfu       *SFUServer
 }
-
-
 
 const (
 	OK = iota
@@ -19,22 +19,22 @@ const (
 
 type SignalRequest struct {
 	RoomID string `json:"roomID"`
-	SDP string `json:"sdp"`
+	SDP    string `json:"sdp"`
 }
 
 type SignalResponse struct {
-	Code int `json:"code"`
-	Msg string `json:"message"`
-	SDP string `json:"sdp"`
+	Code int    `json:"code"`
+	Msg  string `json:"message"`
+	SDP  string `json:"sdp"`
 }
 
-func NewRoomServer(sfu *SFUServer) (*RoomServer, error){
+func NewRoomServer(sfu *SFUServer) (*RoomServer, error) {
 	return &RoomServer{
-		sfu: sfu,
+		sfu:       sfu,
 		ginEngine: gin.Default()}, nil
 }
 
-func (r *RoomServer) Start() error{
+func (r *RoomServer) Start() error {
 
 	var err error
 	//r.ginEngine.Use(TlsHandler())
@@ -45,36 +45,37 @@ func (r *RoomServer) Start() error{
 	r.ginEngine.GET("/player", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "player.html", nil)
 	})
+	r.ginEngine.Use(middleware())
 
 	r.ginEngine.POST("/publish", func(context *gin.Context) {
 		var s SignalRequest
-		 if err = context.BindJSON(&s); err != nil {
-		 	context.JSON(200, SignalResponse{
+		if err = context.BindJSON(&s); err != nil {
+			context.JSON(200, SignalResponse{
 				Code: Fail,
-				Msg: err.Error(),
+				Msg:  err.Error(),
 				SDP:  "",
 			})
-		 } else if answer, err := r.sfu.NewPublish(s.RoomID, s.SDP); err != nil {
-			 context.JSON(200, SignalResponse{
-				 Code: Fail,
-				 Msg:  err.Error(),
-				 SDP:  "",
-			 })
-		 } else {
-		 	context.JSON(200, SignalResponse{
+		} else if answer, err := r.sfu.NewPublish(s.RoomID, s.SDP); err != nil {
+			context.JSON(200, SignalResponse{
+				Code: Fail,
+				Msg:  err.Error(),
+				SDP:  "",
+			})
+		} else {
+			context.JSON(200, SignalResponse{
 				Code: OK,
 				Msg:  "",
 				SDP:  answer,
 			})
-		 }
+		}
 	})
-	
+
 	r.ginEngine.POST("/play", func(context *gin.Context) {
 		var s SignalRequest
 		if err = context.BindJSON(&s); err != nil {
 			context.JSON(200, SignalResponse{
 				Code: Fail,
-				Msg: err.Error(),
+				Msg:  err.Error(),
 				SDP:  "",
 			})
 		} else if answer, err := r.sfu.NewPlay(s.RoomID, s.SDP); err != nil {
@@ -92,13 +93,11 @@ func (r *RoomServer) Start() error{
 		}
 	})
 
-
-	if err = r.ginEngine.RunTLS(":8080", "server.crt", "server.key"); err != nil {
+	if err = r.ginEngine.Run(":8080"); err != nil {
 		return err
 	}
 	return nil
 }
-
 
 //func TlsHandler() gin.HandlerFunc {
 //	return func(c *gin.Context) {
@@ -116,3 +115,27 @@ func (r *RoomServer) Start() error{
 //		c.Next()
 //	}
 //}
+
+func middleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+		c.Set("example", "123456")
+		// c.Next()后就执行真实的路由函数，路由执行完成之后接着走time.Since(t)
+
+		c.Next()
+
+		// 从time.Now()到目前为止过了多长时间
+		latency := time.Since(t)
+		log.Print("--", latency)
+
+		// gin设置响应头，设置跨域
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Header("Access-Control-Allow-Headers", "Action, Module, X-PINGOTHER, Content-Type, Content-Disposition")
+
+		//设置中间件的响应头，路由的响应头可以在路由返回中设置，参考/ping
+		// c.Writer.WriteHeader(http.StatusMovedPermanently)
+		status := c.Writer.Status()
+		log.Println("==", status)
+	}
+}
